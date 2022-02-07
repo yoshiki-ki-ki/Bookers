31c9e3ce8737a8e33c6d1f835770874d4391819b
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-07 10:03:29 +0000

    [Update]新規登録機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,12 @@
 class BooksController < ApplicationController
-
   def index
     @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
-    list.save
-    redirect_to 'books/show'
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
@@ -17,7 +16,9 @@ class BooksController < ApplicationController
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
