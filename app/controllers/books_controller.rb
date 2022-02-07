05244cb9394ced0d2b9d1566e97a15e18f30a13f
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-07 09:33:11 +0000

    [Update]16新規登録難航

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
+
   def index
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    list.save
+    redirect_to 'books/show'
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   get 'books/show'
   get 'books/edit'
-  get '/'=> 'homes#top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
