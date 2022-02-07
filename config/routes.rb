7e0d97dd575c96a180b029e3f37e9f53e3fa99f6
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-07 13:47:11 +0000

    [Update]backボタン追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   root to: 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
