65e49e1eccc7c84a6c835560a94e014406385a10
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-07 14:29:46 +0000

    [Update]destoy機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   root to: 'homes#top'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
