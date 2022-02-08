38d25a99956355538454a629aedc7a5cdb80f486
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-08 06:11:05 +0000

    [Update]バリデーション難航

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,16 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
   
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      redirect_to '/books'
+    end
+  end
+
   def show
     @book = Book.find(params[:id])
   end
@@ -17,13 +20,16 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      redirect_to edit_book_path(@book.id)
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
